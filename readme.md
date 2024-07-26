542504e96f54c9d049b26bcee1b619df18634d14
belassiria aymane <aymanebel2@outlook.fr>
2024-07-26 01:35:54 +0100

    init project

---
+++ readme.md
@@ -0,0 +1,1 @@
+# ClusteredData Warehouse
---
+++ src/test/java/com/datawarehourse/clustered_data/ClusteredDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datawarehourse.clustered_data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClusteredDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
